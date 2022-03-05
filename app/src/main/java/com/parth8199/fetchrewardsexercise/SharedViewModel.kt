be8aee7286c60e46b5ed7b93d408834cc1a1b499
parthjpatel99 <parth8199@gmail.com>
2022-03-04 21:00:22 -0500

    Implemented Simple Cache

--- app/src/main/java/com/parth8199/fetchrewardsexercise/SharedViewModel.kt
+++ app/src/main/java/com/parth8199/fetchrewardsexercise/SharedViewModel.kt
@@ -1,22 +1,34 @@
 package com.parth8199.fetchrewardsexercise
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.parth8199.fetchrewardsexercise.domain.models.ItemList
+import com.parth8199.fetchrewardsexercise.network.ItemListCache
 import com.parth8199.fetchrewardsexercise.network.response.GetListFetchRewards
 import kotlinx.coroutines.launch
 
+private const val TAG = "SharedViewModel"
 class SharedViewModel : ViewModel() {
     private val repository = SharedRepository()
     private val _itemByIdLiveData = MutableLiveData<ItemList?>()
     val itemByIdLiveData: LiveData<ItemList?> = _itemByIdLiveData
 
     fun refreshItem() {
+        val cachedItemList = ItemListCache.itemListCache
+        if (cachedItemList.isNotEmpty()) {
+            _itemByIdLiveData.postValue(cachedItemList)
+            return
+        }
         viewModelScope.launch {
             val response = repository.getItemById()
             _itemByIdLiveData.postValue(response)
+            Log.i(TAG, "Made Network Request")
+            if (response != null) {
+                ItemListCache.itemListCache=response
+            }
         }
     }
 }
---
+++ app/src/main/java/com/parth8199/fetchrewardsexercise/network/ItemListCache.kt
@@ -0,0 +1,7 @@
+package com.parth8199.fetchrewardsexercise.network
+
+import com.parth8199.fetchrewardsexercise.domain.models.ItemList
+
+object ItemListCache  {
+    var itemListCache = ItemList()
+}
